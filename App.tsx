62275c6e0daea1ba5533198a64c17e7ed340f452
marioignacioespinozalobos <152904922+marioignacioespinozalobos@users.noreply.github.com>
2024-04-28 21:20:06 -0400

    main

--- App.tsx
+++ App.tsx
@@ -13,7 +13,7 @@ const App = () => {
       <NavigationContainer>
         <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="VideoCall" component={VideoCallScreen} />
+        <Stack.Screen name="VideoCalls" component={VideoCallScreen} />
         </Stack.Navigator>
       </NavigationContainer>        
     </View>
